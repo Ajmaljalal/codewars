d1e7bf05e8953d8440d2c3f2707f923e0ac79e12
Ajmal Jalal <ajmaljalal58@gmail.com>
2016-02-01 17:10:43 -0800

    codewars challenges initial steps

---
+++ maxicanWaveFunc.js
@@ -0,0 +1,14 @@
+export function wave(str) {
+    const array = []
+    let newString = str
+
+    for (i = 0; i <= str.length; i++) {
+        if (str.split('')[i] && str.split('')[i].trim().length) {
+            const upperCaseChar = newString.split('')[i].toUpperCase()
+            const stringWave = newString.split('')
+            stringWave[i] = upperCaseChar
+            array.push(stringWave.join(''))
+        }
+    }
+    return array
+}
