1985fdbf294f894d5326f2b73816e2bad1fe0e7d
Ajmal Jalal <ajmaljalal58@gmail.com>
2019-02-06 00:00:00 -0800

    maximum sum of a subarray wip

--- maximumSubArraySum.js
+++ maximumSubArraySum.js
@@ -1,8 +1,21 @@
-var maxSequence = function(arr){
-  if(!arr.length) return 0
-  
-  if(!arr.join('').includes('-')) {
-    const sum = arr.reduce((result, number) => result + number, 0)
-    return sum
-  }
+var maxSequence = function (arr) {
+    let maxSum = 0
+    if (!arr.length) return maxSum
+
+    if (!arr.join('').includes('-')) {
+        maxSum = arr.reduce((result, number) => result + number, 0)
+        return maxSum
+    }
+
+    arr.forEach(number => {
+        if (!number.toString().includes('-')) {
+            maxSum = 0
+        }
+    })
+
+
+
+    return maxSum
+
+
 }
